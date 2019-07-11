b2518269713e435aa3a78c7c818948c78323ab21
Felipe Gonçalves <felribsg@gmail.com>
2019-07-11 19:20:19 -0300

    Fixed indentation

--- create-backup.php
+++ create-backup.php
@@ -10,15 +10,10 @@ if (!touch($maintenanceFile)) {
 }
 
 function execute($command) {
-	exec($command);
+    exec($command);
 }
 
 $backupDir = __DIR__ . '/backups';
-if (!is_dir($backupDir)) {
-    if (!mkdir($backupDir, 0777)) {
-        die('impossible to create backup dir');
-    }
-}
 
 $time = gmdate('Ymd_His');
 $user = (string) $config->global->resources->default_setup->connection->username;
@@ -29,60 +24,60 @@ $db   = (string) $config->global->resources->default_setup->connection->dbname;
 $file = $time.'_GMT_'.$db;
 
 $ignore_tables = [
-	'aw_core_logger',
-	'bss_index',
-	'catalog_category_flat_store_1',
-	'catalog_category_product_index',
-	'catalog_product_flat_1',
-	'catalog_product_index_eav',
-	'catalog_product_index_eav_decimal',
-	'catalog_product_index_eav_decimal_idx',
-	'catalog_product_index_eav_decimal_tmp',
-	'catalog_product_index_eav_idx',
-	'catalog_product_index_eav_tmp',
-	'catalog_product_index_group_price',
-	'catalog_product_index_price',
-	'catalog_product_index_price_idx',
-	'cataloginventory_stock_status',
-	'cataloginventory_stock_status_idx',
-	'cataloginventory_stock_status_tmp',
-	'catalogsearch_fulltext',
-	'catalogsearch_query',
-	'catalogsearch_result',
-	'core_url_rewrite',
-	'cron_schedule',
-	'customgrid_grid',
-	'customgrid_grid_column',
-	'index_event',
-	'index_process_event',
-	'interagynet_bairro',
-	'interagynet_endereco',
-	'interagynet_pagseguro_notification',
-	'interagynet_pagseguro_transaction',
-	'log_customer',
-	'log_quote',
-	'log_summary',
-	'log_summary_type',
-	'log_url',
-	'log_url_info',
-	'log_visitor',
-	'log_visitor_info',
-	'log_visitor_online',
-	'report_compared_product_index',
-	'report_event',
-	'report_event_types',
-	'report_viewed_product_aggregated_daily',
-	'report_viewed_product_aggregated_monthly',
-	'report_viewed_product_aggregated_yearly',
-	'report_viewed_product_index',
-	'sales_bestsellers_aggregated_daily',
-	'sales_bestsellers_aggregated_monthly',
-	'sales_bestsellers_aggregated_yearly',
+    'aw_core_logger',
+    'bss_index',
+    'catalog_category_flat_store_1',
+    'catalog_category_product_index',
+    'catalog_product_flat_1',
+    'catalog_product_index_eav',
+    'catalog_product_index_eav_decimal',
+    'catalog_product_index_eav_decimal_idx',
+    'catalog_product_index_eav_decimal_tmp',
+    'catalog_product_index_eav_idx',
+    'catalog_product_index_eav_tmp',
+    'catalog_product_index_group_price',
+    'catalog_product_index_price',
+    'catalog_product_index_price_idx',
+    'cataloginventory_stock_status',
+    'cataloginventory_stock_status_idx',
+    'cataloginventory_stock_status_tmp',
+    'catalogsearch_fulltext',
+    'catalogsearch_query',
+    'catalogsearch_result',
+    'core_url_rewrite',
+    'cron_schedule',
+    'customgrid_grid',
+    'customgrid_grid_column',
+    'index_event',
+    'index_process_event',
+    'interagynet_bairro',
+    'interagynet_endereco',
+    'interagynet_pagseguro_notification',
+    'interagynet_pagseguro_transaction',
+    'log_customer',
+    'log_quote',
+    'log_summary',
+    'log_summary_type',
+    'log_url',
+    'log_url_info',
+    'log_visitor',
+    'log_visitor_info',
+    'log_visitor_online',
+    'report_compared_product_index',
+    'report_event',
+    'report_event_types',
+    'report_viewed_product_aggregated_daily',
+    'report_viewed_product_aggregated_monthly',
+    'report_viewed_product_aggregated_yearly',
+    'report_viewed_product_index',
+    'sales_bestsellers_aggregated_daily',
+    'sales_bestsellers_aggregated_monthly',
+    'sales_bestsellers_aggregated_yearly',
 ];
 if ($ignore_tables) {
-	$ignore_tables = " --ignore-table=$db.".implode(" --ignore-table=$db.", $ignore_tables);
+    $ignore_tables = " --ignore-table=$db.".implode(" --ignore-table=$db.", $ignore_tables);
 } else {
-	$ignore_tables = '';
+    $ignore_tables = '';
 }
 
 // backups de toda a estrutura das tabelas
